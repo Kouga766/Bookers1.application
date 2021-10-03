0f2fb1716f1676ed3b10eb8afb015305a1cc16ad
EC2 Default User <ec2-user@ip-172-31-51-152.ec2.internal>
2021-10-03 09:52:10 +0000

    修正完了

--- bookers1/app/controllers/books_controller.rb
+++ bookers1/app/controllers/books_controller.rb
@@ -13,6 +13,8 @@ class BooksController < ApplicationController
       flash[:notice] = "Book was successfully created."
       redirect_to book_path(@book.id)
     else
+      @books=Book.all
+      
       render :index
     end
   end
@@ -22,15 +24,17 @@ class BooksController < ApplicationController
   end
 
   def edit
+    @books = Book.all
     @book = Book.find(params[:id])
   end
 
   def update
     @book = Book.find(params[:id])
-    if @book.update()
+    if @book.update(book_params)
       flash[:notice] = "Book was successfully updated."
       redirect_to book_path(@book.id)
     else
+      
       render :edit
     end
   end
